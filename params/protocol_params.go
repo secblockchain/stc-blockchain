3e3a14f2f272638e1f2ba430534bbf56703078f6
santo-smartenergypay <santo@smartenergypay.com>
2026-08-21 16:12:41 +0900

    refactor: simplify base fee calculation in EIP-1559 implementation and introduce TransferBaseFee constant

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -127,6 +127,7 @@ const (
 	DefaultBaseFeeChangeDenominator = 8          // Bounds the amount the base fee can change between blocks.
 	DefaultElasticityMultiplier     = 2          // Bounds the maximum gas limit an EIP-1559 block may have.
 	InitialBaseFee                  = 1000000000 // Initial base fee for EIP-1559 blocks.
+	TransferBaseFee                 = 2e18       // Transfer base fee for EIP-1559 blocks.
 
 	MaxCodeSize     = 24576           // Maximum bytecode to permit for a contract
 	MaxInitCodeSize = 2 * MaxCodeSize // Maximum initcode to permit in a creation transaction and create instructions
